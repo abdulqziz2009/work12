9da784956330f888dbfe389a917f54b0cc07fc9d
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 12:58:00 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phrase = "Пример текста";
+
+        final int num = 10;
+
+        String word = "слово";
+
+        String result = num + " " + word;
+
+        System.out.println("Значение переменной phrase: " + phrase);
+        System.out.println("Значение константы num: " + num);
+        System.out.println("Значение переменной word: " + word);
+        System.out.println("Результат конкатенации: " + result);
+
+        if (num < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (num > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили нуль");
+        }
     }
 }
